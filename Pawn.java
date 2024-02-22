30d3c2e88b24c2298d70194610470c119c511931
Christopher Gonzalez <cbg65@scarletmail.rutgers.edu>
2024-02-22 16:37:00 -0500

    fixed bit on Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -1,41 +1,57 @@
 package chess;
 
-import java.util.Map;
-
 public class Pawn extends Piece {
 
-    public static boolean isValidMove(Map<String, ReturnPiece> board, String from, String to, ReturnPiece.PieceType pieceType) {
+    public boolean validMove(String from, String to) {
+
+        if (board.containsKey((from))){
+            return false;
+        }
         //grabbing current location
-        PieceFile fromFile = PieceFile.valueOf(from.substring(0, 1));
-        int fromRank = Integer.parseInt(from.substring(1));
+        ReturnPiece start = board.get(from);
 
         //grabbing next location 
-        PieceFile toFile = PieceFile.valueOf(to.substring(0, 1));
-        int toRank = Integer.parseInt(to.substring(1));
+        ReturnPiece destination= board.get(to);
 
-        //if direction is 1, the piece is White so we move up rank
-        //if direction is -1, the piece is Black so we move down rank
-        int direction = pieceType.toString().startsWith("W") ? 1 : -1;
+        //if pieceType oridnal is 0-5, it is white
+        //if pieceType ordinal is 6-11, it is black
+        int direction = 0;
+        if (start.pieceType.ordinal()<6){
+            direction= 1; //direction will move forward
+        }
+        else{
+            direction = -1;
+        }
+
+        //check if it their first move
+        if (start.pieceRank == 2){
+            //they are are to move one or two spaces up
+            // if (Math.abs(destination.pieceFile.ordinal()-start.pieceFile.ordinal()) )
+        }
 
         // Check for a valid one-square forward move
-        if (fromFile == toFile && ((toRank - fromRank) == direction)) {
+        if (destination.pieceFile == start.pieceFile && ((destination.pieceRank - destination.pieceRank) == direction)) {
             // The target space must be empty
-            String toPosition = toFile.toString() + toRank;
+            String toPosition = destination.pieceFile.toString() + destination.pieceRank;
             return !board.containsKey(toPosition);
         }
 
         // Check for a valid capture move (one square diagonally)
-        if (Math.abs(fromFile.ordinal() - toFile.ordinal()) == 1 && (toRank - fromRank) == direction) {
+        if (Math.abs(destination.pieceFile.ordinal() - destination.pieceFile.ordinal()) == 1 && (start.pieceRank - start.pieceRank) == direction) {
             // There must be an opponent's piece at the target space
-            String toPosition = toFile.toString()+ toRank;
+            String toPosition = destination.pieceFile.toString()+ destination.pieceRank;
             ReturnPiece targetPiece = board.get(toPosition);
             //if the piece is there and not the same color
-            if (targetPiece != null && targetPiece.pieceType != pieceType) {
+            if (targetPiece != null && targetPiece.pieceType != start.pieceType) {
                 return true;
             }
         }
 
         //if everything fails
+        return false;
+    }
+
+    public boolean move(String from , String to){
         return false;
     }
 }
